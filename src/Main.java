f0dec27d6d6643294c35bf1a55f2bc9778389058
David Molta <dmh1802@gmail.com>
2023-04-17 13:35:55 -0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.menu();
+    }
+
+
+    public void menu(){
+
+       do{
+           int option = 0;
+
+           System.out.println("Welcome to the Avianca Airline assistant" + "\n"
+           "1.");
+
+
+       }while(option!=0);
+
+
+    }
+}
+
+
